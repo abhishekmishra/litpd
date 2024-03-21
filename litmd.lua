08a01c350ce50bbd63c8acd1f2e4836dca689d93
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-21 19:13:29 +0530

    litmd.lua initial implementation complete

--- litmd.lua
+++ litmd.lua
@@ -7,25 +7,60 @@
 -- get the arguments from the command line
 local args = {...}
 
--- if no arguments are provided, print the usage
-if #args == 0 then
-  print("Usage: litmd <weave|tangle> <inputfile.md> [options]")
-  return
+-- get the path of script, and its directory
+local script_path = arg[0]
+-- replace all backslashes with forward slashes
+script_path = script_path:gsub("\\", "/")
+local litmd_home = script_path:match(".*/")
+-- print("litmd_home: " .. litmd_home)
+
+--- Show usage
+local function show_usage()
+  print("Usage: litmd <inputfile.md> [options]")
 end
 
--- check if "weave" or "tangle" is provided as the first argument
-local mode = args[1]
-if mode ~= "weave" and mode ~= "tangle" then
-  print("Invalid mode. Use 'weave' or 'tangle'")
+-- if no arguments are provided, print the usage
+if #args == 0 then
+  show_usage()
   return
 end
 
 -- get the input file name
-local input_file = args[2]
+local input_file = args[1]
 if input_file == nil then
   print("No input file provided")
+  show_usage()
   return
 end
 
-local TANGLE_FILTER = "mdtangle.lua"
-local PANDOC_CMD = "pandoc --lua-filter=" .. TANGLE_FILTER .. " --from=markdown --to=html --standalone"
+-- get the rest of the arguments
+local options = {}
+for i = 2, #args do
+  table.insert(options, args[i])
+end
+
+local TANGLE_FILTER = litmd_home .. "mdtangle.lua"
+local PANDOC_CMD = "pandoc --lua-filter=" .. TANGLE_FILTER .. " --from=markdown "
+
+-- create the final command, start with the pandoc command
+local cmd = PANDOC_CMD
+-- add the input file
+cmd = cmd .. input_file
+-- add the rest of the options
+for i = 1, #options do
+  cmd = cmd .. " " .. options[i]
+end
+
+-- display the command to be executed
+print("Executing: " .. cmd)
+
+-- execute the command
+local handle = io.popen(cmd)
+if handle == nil then
+  print("Error executing command")
+  return
+end
+local result = handle:read("*a")
+handle:close()
+-- handle the result
+print(result)
